54e19bee4da47a47106f997b470c16262f7c7a4f
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2021-04-29 13:56:12 -0500

    se creo el servicio

--- src/main/java/com/bcom/apibcomevents/evento/dominio/servicio/ControladorServicioEvento.java
+++ src/main/java/com/bcom/apibcomevents/evento/dominio/servicio/ControladorServicioEvento.java
@@ -1,4 +1,16 @@
 package com.bcom.apibcomevents.evento.dominio.servicio;
 
+import com.bcom.apibcomevents.evento.dominio.dto.EventoDTO;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ControladorServicioEvento {
+
+    Optional<EventoDTO> agregarUnEventoConUsuario(EventoDTO eventoDTO, Long idUsuario);
+
+    List<EventoDTO> listarEventos();
+
+    Optional<EventoDTO> actualizarEvento(EventoDTO eventoDTO,Long idEvento);
+
 }
