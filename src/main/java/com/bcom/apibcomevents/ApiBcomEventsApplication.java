c2578eae689c7ab4d76d7f303834f92ec3748a91
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2021-04-29 11:32:00 -0500

    se crearon la entidad

---
+++ src/main/java/com/bcom/apibcomevents/ApiBcomEventsApplication.java
@@ -0,0 +1,13 @@
+package com.bcom.apibcomevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBcomEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBcomEventsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/bcom/apibcomevents/usuario/infraestructura/entidad/Usuario.java
@@ -0,0 +1,27 @@
+package com.bcom.apibcomevents.usuario.infraestructura.entidad;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table( name = "usuario")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @Column( name = "fecha_creacion" )
+    private Date fechaCreacion;
+
+    @Column ( name="fecha_modificacion")
+    private Date fechaModificacion;
+}
