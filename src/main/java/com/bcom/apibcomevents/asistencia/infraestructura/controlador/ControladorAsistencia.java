3c33b835d42c097342dda013854a497519977c81
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2021-04-30 16:53:04 -0500

    se adicionaron los cross para las peticiones al cliente

--- src/main/java/com/bcom/apibcomevents/asistencia/infraestructura/controlador/ControladorAsistencia.java
+++ src/main/java/com/bcom/apibcomevents/asistencia/infraestructura/controlador/ControladorAsistencia.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("asistencia")
+@CrossOrigin(origins = "http://localhost:8081")
 @AllArgsConstructor
 public class ControladorAsistencia {
 
--- src/main/java/com/bcom/apibcomevents/evento/infraestructura/controlador/ControladorEvento.java
+++ src/main/java/com/bcom/apibcomevents/evento/infraestructura/controlador/ControladorEvento.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8081")
 @AllArgsConstructor
 public class ControladorEvento {
 
